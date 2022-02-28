d3a2ac27537078c6054124c0a6a4dfacbe140ffc
aprabh92 <aprabh@juniper.net>
2022-02-28 16:49:08 -0500

    added readme

--- go-container/test-topology.sh
+++ go-container/test-topology.sh
@@ -5,8 +5,8 @@
 # -------------------------------- #
 create()
 {
-    docker run -itd --privileged --name go1 -v ${PWD}:/app alpine:go
-    docker run -itd --privileged --name go2 -v ${PWD}:/app alpine:go
+    docker run -itd --privileged --name go1 -v ${PWD}:/app go-packet-crafter:v1.0
+    docker run -itd --privileged --name go2 -v ${PWD}:/app go-packet-crafter:v1.0
     docker network create -d bridge go1_go2
     docker network connect go1_go2 go1
     docker network connect go1_go2 go2
